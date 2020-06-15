c75c5a8f248a53447678c1f374e302dd8cbf6008
abimimi67 <alibinapi@gmail.com>
2020-06-15 06:16:30 +0200

    Updates

---
+++ precache-manifest.281f6827a28ab67a5b48aa9e818d008d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7d13ceaff67e9905fcae3af4c03a49",
+    "url": "/studentslist/index.html"
+  },
+  {
+    "revision": "30419370ce6380700b54",
+    "url": "/studentslist/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bdaea0444276b960021f",
+    "url": "/studentslist/static/js/2.3c665a38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/studentslist/static/js/2.3c665a38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30419370ce6380700b54",
+    "url": "/studentslist/static/js/main.78fbaf55.chunk.js"
+  },
+  {
+    "revision": "d46a807a5ab91f57b566",
+    "url": "/studentslist/static/js/runtime-main.a261d80f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/studentslist/static/media/logo.5d5d9eef.svg"
+  }
+]);
